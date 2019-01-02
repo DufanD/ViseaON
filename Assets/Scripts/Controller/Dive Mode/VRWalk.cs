8ed9c652b9477f02615db84227f08b237e6199a2
DufanD <33487607+DufanD@users.noreply.github.com>
2019-01-02 11:37:22 +0700

    Update VRWalk.cs

--- Assets/Scripts/Controller/Dive Mode/VRWalk.cs
+++ Assets/Scripts/Controller/Dive Mode/VRWalk.cs
@@ -6,9 +6,6 @@ public class VRWalk : MonoBehaviour {
     public float speed = 3.0f;
     public bool moveForward = true;
 
-    private float vertical;
-    private float horizontal;
-
     // Update is called once per frame
     void Update()
     {
@@ -17,17 +14,7 @@ public class VRWalk : MonoBehaviour {
             Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
 
             Vector3 moving = vrCamera.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
-            //if (Mathf.Abs(vertical) > 0.01)
-            //{
-            //    //move in the direction of the camera
-            //    transform.position = transform.position + forward * vertical * speed * Time.deltaTime;
-            //}
-            //if (Mathf.Abs(horizontal) > 0.01)
-            //{
-            //    //strafe sideways
-            //    transform.position += new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
-            //}
-            //transform.position = transform.position + forward * speed * Time.deltaTime;
+            
             transform.position = transform.position + moving * speed * Time.deltaTime;
         }
     }
